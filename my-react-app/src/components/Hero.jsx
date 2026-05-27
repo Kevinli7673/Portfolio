9f9a4cb6f0bdd8f2b5865ba2d3aa105b10277f68
Kevinli7673 <kevin.lizz7673@gmail.com>
2026-05-27 17:19:29 -0400

    Initialize file - Completed most of the Nav and started on Hero section

---
+++ my-react-app/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+
+
+function  Hero() {
+    return(
+        <>
+            <p>
+                Welcome to <br/>
+                <span>my place</span>, <br/>
+                Pull up a chair.
+            </p>
+            <p>
+                Hi, I'm Kevin. This is the corner of the internet where I <br/>
+                share my projects, experience, and other exciting stuff. <br/>
+                Stay here as long as you like.
+            </p>
+        </>
+    )
+}
+
+export default Hero
---
+++ my-react-app/src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import Linkedin from "../assets/Linkedin logo.svg"
+import Github from "../assets/Github logo.svg"
+import Resume from "../assets/resume logo.svg"
+import myResume from "../assets/resume 2029.pdf"
+import "./Nav.css"
+
+function Nav() {
+    return (
+        <>
+            <nav className="Nav">
+                <p className="name">Kevin Li</p>
+                <ul className="mainNav">
+                    <li><a>Home</a></li>
+                    <li><a>Experience</a></li>
+                    <li><a>Projects</a></li>
+                </ul>
+                <ul className="Links">
+                    <li><a href="https://www.linkedin.com/in/kevin-li7673/" target="_blank"><img className="linkedin" src={Linkedin} alt="Linkedin logo"/></a></li>
+                    <li><a href="https://github.com/Kevinli7673" target="_blank"><img className="Github" src={Github} alt="Github logo"/></a></li>
+                    <li><a href={myResume} target="_blank"><img className="Resume" src={Resume} alt="Resume logo"/></a></li>
+                </ul>
+            </nav>
+            <hr/>
+        </>
+    );
+}
+
+export default Nav
