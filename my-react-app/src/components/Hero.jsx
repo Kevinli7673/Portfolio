0f8192d83a64832c503595df7527e730366ae258
Kevinli7673 <kevin.lizz7673@gmail.com>
2026-05-28 23:39:56 -0400

    Finished Hero section (beside rotating text), added the new font

--- my-react-app/src/components/Hero.jsx
+++ my-react-app/src/components/Hero.jsx
@@ -1,18 +1,31 @@
-
+import Campfire from "../assets/campfire.gif"
+import "./Hero.css"
 
 function  Hero() {
     return(
         <>
-            <p>
-                Welcome to <br/>
-                <span>my place</span>, <br/>
-                Pull up a chair.
-            </p>
-            <p>
-                Hi, I'm Kevin. This is the corner of the internet where I <br/>
-                share my projects, experience, and other exciting stuff. <br/>
-                Stay here as long as you like.
-            </p>
+            <div className="Hero">
+                <div className="leftSide">
+                    <div className="heroQuote">
+                        <p>
+                            Welcome to <br/>
+                            <span>my place</span>, <br/>
+                            Pull up a chair.
+                        </p>
+                    </div>
+                    <div className="Caption">
+                        <p>
+                            Hi, I'm Kevin. This is the corner of the internet where I <br/>
+                            share my projects, experience, and other exciting stuff. <br/>
+                            Stay here as long as you like.
+                        </p>
+                    </div>
+                </div>
+                <div className="rightSide">
+                    <img src={Campfire} alt="Campfire"/>
+                    <p>Rotating text...</p>
+                </div>
+            </div>
         </>
     )
 }
