d5b3005ca8f497dd8728f195d3d5f67d6c4d1353
kevin li <kevin.lizz7673@gmail.com>
2026-06-07 18:52:04 -0400

    Experience: finish props/css (still need adjustment)

--- my-react-app/src/components/Experience.jsx
+++ my-react-app/src/components/Experience.jsx
@@ -1,53 +1,86 @@
 import './Experience.css'
+import IST from '../assets/IST.jpg'
+import KH from '../assets/KH-logo.png'
 
-function ExperienceCard({Role, Company, Date, Location, Summary, Details, Tags}) {
+function ExperienceCard({Image, Role, Company, Date, Location, Summary, Details, Tags}) {
     return (
-        <div className={Company}>
-            <div className='Role'>
-                <span>{Role}</span>
+        <>
+            <div className='Experience-card'>
+                <div className='Left'>
+                    <div className='Image'>
+                        <img src={Image} alt='Company Logo' />
+                    </div>
+                </div>
+                <div className='Middle'>
+                    <div className='Role'>
+                        <span>{Role}</span>
+                    </div>
+                    <div className='Company'>
+                        <span>{Company}</span>
+                    </div>
+                    <div className='Location'>
+                        <span>{Location}</span>
+                    </div>
+                    <div className='Summary'>
+                        <p>{Summary}</p>
+                    </div>
+                    <div className='Detail'>
+                        {Details.map(detail => <p key={detail} className='details'><span className='Arrow'>→ </span>{detail}</p>)}
+                    </div>
+                    <div className='Tags'>
+                        {Tags.map(tag => (<span key={tag} className='tags'>{tag}</span>))}
+                    </div>
+                    <div className='Date'>
+                        <span>{Date}</span>
+                    </div>
+                </div>
             </div>
-            <div>
-                <span>{Company}</span>
-            </div>
-            <div className='Date'>
-                <span>{Date}</span>
-            </div>
-            <div>
-                <span>{Location}</span>
-            </div>
-            <div className='Sumary'>
-                <p>{Summary}</p>
-            </div>
-            <div className='Detail'>
-                {Details.map(detail => <p key={detail}>{detail}</p>)}
-            </div>
-            <div className='Tags'>
-                {Tags.map(tag => (<span key={tag}>{tag}</span>))}
-            </div>
-            <br/>
-        </div>
+                <hr className='Bottom_hr' />
+        </>
     )
 }
 
 function Experience() {
     const experiences = [
         {
+            Image: IST,
             Role: "Software Engineer Intern",
             Company: "UCF - Insitute For Simulation & Training",
             Date: "June - July 2026",
             Location: "Orlando, Fl",
             Summary: "Developing a real-time Unity VR simulator that visualizes ionizing radiation through cloud-chamber particle tracks.",
             Details: [
-                "→ Blah blah blah blah blah blah blah blah blah blah blah blah",
-                "→ sdfsdkljsdf sdflksdjfsdlfkjsdf sdflkjsdfljsdf sdflkjsdfljsdf",
-                "→ sdf sdflkjsdflkjsdf sdflkjsdfljk sdflkjsdfl sdflkjsdfl sdflkj"
+                "Built a real-time VR radiation simulator in Unity for Meta Quest 3, rendering accurate alpha, beta, and gamma particle trails at [90] FPS.",
+                "Engineered a dual-perspective system (macro table view and first-person chamber view) with seamless scaling and real-time switching across [4+] radiation sources.",
+                "Delivered an open-source AR/VR educational prototype with a [4]-person team for ORAU to demonstrate radiation physics interactively."
                 ],
             Tags: ["Unity", "Virtual Reality", "C#"]
-        }
+        },
+        {
+            Image: KH,
+            Role: "Outreach Team",
+            Company: "KnightHacks",
+            Date: "January 2026 - Present",
+            Location: "Orlando, Fl",
+            Summary: "Developing a real-time Unity VR simulator that visualizes ionizing radiation through cloud-chamber particle tracks.",
+            Details: [
+                "Built a real-time VR radiation simulator in Unity for Meta Quest 3, rendering accurate alpha, beta, and gamma particle trails at [90] FPS.",
+                "Engineered a dual-perspective system (macro table view and first-person chamber view) with seamless scaling and real-time switching across [4+] radiation sources.",
+                "Delivered an open-source AR/VR educational prototype with a [4]-person team for ORAU to demonstrate radiation physics interactively."
+                ],
+            Tags: ["Unity", "Virtual Reality", "C#"]
+        },
     ]
     return(
-        <div className='Experience'>
-            {experiences.map(experience => (<ExperienceCard key={experience.Role} {...experience} />))}
+        <div className='experience'>
+            <div className='Title'>
+                <span>Experience</span>
+            </div>
+            <div className='caption'>
+                <p>My Professional Journey: Growth Through Internships and Leadership</p>
+            </div>
+            <hr/>
+            {experiences.map(experience => (<ExperienceCard key={experience.Image} {...experience} />))}
         </div>
     )
 }
