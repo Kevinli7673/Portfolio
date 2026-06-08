55f90b7b7ca17868b0cad80cf20b005eb9978dda
Kevinli7673 <kevin.lizz7673@gmail.com>
2026-06-08 10:36:36 -0400

    Update Experience.jsx / Implement ScrollToTop

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,11 +4,13 @@ import Nav from "./components/Nav.jsx"
 import Hero from "./components/Hero.jsx"
 import Projects from "./components/Projects.jsx"
 import Experience from "./components/Experience.jsx"
+import ScrollToTop from "./components/ScrollToTop.jsx"
 
 function App() {
   return (
     <>
       <Nav />
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<><Hero /><Projects /></>} />
         <Route path="/experience" element={<Experience />} />
--- my-react-app/src/components/Experience.jsx
+++ my-react-app/src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import './Experience.css'
 import IST from '../assets/IST.jpg'
 import KH from '../assets/KH-logo.png'
+import GPK from '../assets/gpk-logo.png'
 
 function ExperienceCard({Image, Role, Company, Date, Location, Summary, Details, Tags}) {
     return (
@@ -45,16 +46,30 @@ function Experience() {
         {
             Image: IST,
             Role: "Software Engineer Intern",
-            Company: "UCF - Insitute For Simulation & Training",
-            Date: "June - July 2026",
+            Company: "UCF - Institute For Simulation & Training",
+            Date: "June 2026 - Present",
             Location: "Orlando, Fl",
             Summary: "Developing a real-time Unity VR simulator that visualizes ionizing radiation through cloud-chamber particle tracks.",
             Details: [
-                "Built a real-time VR radiation simulator in Unity for Meta Quest 3, rendering accurate alpha, beta, and gamma particle trails at [90] FPS.",
-                "Engineered a dual-perspective system (macro table view and first-person chamber view) with seamless scaling and real-time switching across [4+] radiation sources.",
+                "Built a real-time VR radiation simulator in Unity for Meta Quest 3, rendering accurate alpha, beta, and gamma particle trails at 90 FPS.",
+                "Engineered a dual-perspective system (macro table view and first-person chamber view) with seamless scaling and real-time switching across 4+ radiation sources.",
                 "Delivered an open-source AR/VR educational prototype with a [4]-person team for ORAU to demonstrate radiation physics interactively."
                 ],
             Tags: ["Unity", "Virtual Reality", "C#"]
+        },
+        {
+            Image: GPK,
+            Role: "Treasurer",
+            Company: "Graphic Programming Knights",
+            Date: "May 2026 - Present",
+            Location: "Orlando, Fl",
+            Summary: "Manage finances for GPK, a CS RSO serving 160+ UCF students, overseeing club budget, expense reports, and event reimbursements.",
+            Details: [
+                "Led procurement of 100+ branded club t-shirts, negotiating vendor pricing and coordinating design selection, ordering, and member distribution",
+                "Manage event budgets for 5+ workshops and socials per semester, coordinating with officers to fund food, prizes, and materials within budget.",
+                "Developed the club's website from scratch with TypeScript, Next.js, and Three.js improving member access to schedules, workshops, and resources."
+                ],
+            Tags: ["Finance & Budgeting", "Web Development", "Leadership"]
         },
         {
             Image: KH,
@@ -62,15 +77,15 @@ function Experience() {
             Company: "KnightHacks",
             Date: "January 2026 - Present",
             Location: "Orlando, Fl",
-            Summary: "Developing a real-time Unity VR simulator that visualizes ionizing radiation through cloud-chamber particle tracks.",
+            Summary: "Create and edit promotional videos and skits to grow KnightHacks' presence, and represent the RSO at events through tabling and in-person outreach to engage students and build community.",
             Details: [
-                "Built a real-time VR radiation simulator in Unity for Meta Quest 3, rendering accurate alpha, beta, and gamma particle trails at [90] FPS.",
-                "Engineered a dual-perspective system (macro table view and first-person chamber view) with seamless scaling and real-time switching across [4+] radiation sources.",
-                "Delivered an open-source AR/VR educational prototype with a [4]-person team for ORAU to demonstrate radiation physics interactively."
+                "Represent Knight Hacks at 3+ tabling events, pitching the RSO to students and incoming freshmen to grow campus engagement.",
+                "Drove 25,000+ Instagram views through targeted event announcements and multi-channel marketing campaigns."
                 ],
-            Tags: ["Unity", "Virtual Reality", "C#"]
+            Tags: ["Public Speaking", "Content Creation", "Video Editing"]
         },
     ]
+
     return(
         <div className='experience'>
             <div className='Title'>
